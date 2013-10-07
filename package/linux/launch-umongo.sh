4d9d20f4e89a9c82ac5e0e216935e06d5f2a31ae
agirbal <antoine@10gen.com>
2013-10-06 18:53:37 -0700

    - issue50

--- package/linux/launch-umongo.sh
+++ package/linux/launch-umongo.sh
@@ -5,6 +5,5 @@ cd $dir
 if [ -z $minHeap ]; then minHeap=64; fi
 if [ -z $maxHeap ]; then maxHeap=512; fi
 
-# sometimes runs better with opengl forced, but also has visual bugs: -Dsun.java2d.opengl=true
-java -classpath './lib/*.jar' -Xms${minHeap}M -Xmx${maxHeap}M  -Djava.util.logging.config.file=./logging.properties -jar umongo.jar
+java -classpath './lib/*.jar' -Xms${minHeap}M -Xmx${maxHeap}M -jar umongo.jar
 
